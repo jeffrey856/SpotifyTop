43342e539d05549c1dfb0273419ef8e360b71d29
Jeffrey Wu <jwu157@ucsc.edu>
2019-05-15 11:24:47 -0700

    Updated Line Charts

--- spotifytop/src/components/Linechart.js
+++ spotifytop/src/components/Linechart.js
@@ -1,48 +1,26 @@
 import {Line} from 'react-chartjs-2';
 import React,{ Component } from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
+import * as math from 'mathjs'
+
+
 
 const spotifyApi = new SpotifyWebApi();
 
 
-const data = {
-  labels: ['Jan', 'Feb', 'March', 'April', 'March'],
-  datasets: [
-    {
-      label: 'loudness',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: '#1DB954',
-      borderColor: '#1DB954',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: '#1DB954',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: '#1DB954',
-      pointHoverBorderColor: '#1DB954',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
+
+
 
 export class DoughnutExample extends Component{
 	constructor(){
 		super();
 
 		this.state = {
-      pitches: [],
-      data : {
-        labels: [],
+     
+        labels: ['init'],
         datasets: [
           {
-            label: 'loudness',
+            label: 'ptich',
             fill: false,
             lineTension: 0.1,
             backgroundColor: '#1DB954',
@@ -60,10 +38,10 @@ export class DoughnutExample extends Component{
             pointHoverBorderWidth: 2,
             pointRadius: 1,
             pointHitRadius: 10,
-            data: []
+            data: [0,0,0]
           }
         ]
-      }
+      
 		}
 	}
 	componentDidMount(){
@@ -71,25 +49,41 @@ export class DoughnutExample extends Component{
 			spotifyApi.getAudioAnalysisForTrack(this.props.trackProp)
     ])
     
+    
+
 		.then((features) => {
-      var data = this.state.data
-      data.push(features[0].segments)
-			this.setState({
-        pitches: features[0].segments,
-        data
-        })
+      var oldDataSet = this.state;
+      var pitches = [];
+      var time = [];
+      for (var x = 0; x< features[0].segments.length; x +=30){
+        pitches.push(math.mean(features[0].segments[x].pitches).toFixed(2))
+      }
+
+      for (var x = 0; x< features[0].segments.length; x+= 30){
+         time.push((features[0].segments[x].start).toFixed(2))
+      }
+
+      var newDataSet = {
+        ...oldDataSet
+      };
+
+      newDataSet.datasets[0].data = pitches
+      newDataSet.labels = time
+
+			this.setState(newDataSet)
 		
 		})
-	}
+  }
+  
+
 	
   render() {
-    const {pitches} = this.state
-    console.log(pitches)
-    console.log(data)
+    
+    console.log(this.state)    
     return (
       <div>
-        <h2>Line Example</h2>
-        <Line data ={data} />
+        <h2>Pitches</h2>
+        <Line data ={this.state} />
       </div>
     );
   }
